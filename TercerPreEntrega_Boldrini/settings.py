d3004bec079035b0323ca1bcafd947c239795d5a
lucho <you@example.com>
2023-10-27 21:28:48 -0300

    revisar error de extends con base e inicio

--- TercerPreEntrega_Boldrini/settings.py
+++ TercerPreEntrega_Boldrini/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'inicio'
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'TercerPreEntrega_Boldrini.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
